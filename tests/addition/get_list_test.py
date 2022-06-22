4d7aadb33052a07f7020d92a9ad57280126affd0
Edward Seim <sirseim@gmail.com>
2022-06-22 08:54:00 -0700

    setup basic test against list endpoint

---
+++ tests/addition/get_list_test.py
@@ -0,0 +1,17 @@
+import pytest
+from rest_framework import status
+from rest_framework.reverse import reverse
+
+from addition import enums
+from tests import test_constants
+
+
+@pytest.mark.django_db
+def test_get_list(api_client):
+    response = api_client.get(reverse("addition-list"))
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data.get("count") == len(test_constants.TORRENT_DICT)
+    assert response.data.get("results") == [
+        {"id": torrent.id, "state": enums.State.DOWNLOADING, "name": torrent.name, "progress": torrent.progress}
+        for torrent in test_constants.TORRENT_DICT.values()
+    ]
